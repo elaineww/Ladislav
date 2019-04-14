5b7e07e4e1c240d5bd69a39b564f797d23430f3d
Elaine Wang <elaine.wang07@gmail.com>
2019-04-14 00:25:18 -0700

    initial commit

---
+++ assets/js/functions.js
@@ -0,0 +1,93 @@
+$( document ).ready(function() {
+
+  $('.side-nav li').click(function(){
+
+    if (!($(this).hasClass('is-active'))) {
+
+      var $this = $(this),
+          curActive = $this.parent().find('.is-active'),
+          curPos = $this.parent().children().index(curActive),
+          nextPos = $this.parent().children().index($this),
+          lastItem = $(this).parent().children().length - 1;
+
+      updateNavs(nextPos);
+      updateContent(curPos, nextPos, lastItem);
+
+    }
+  });
+
+  // swipe support for touch devices
+  var targetElement = document.getElementById('viewport'),
+      mc = new Hammer(targetElement);
+  mc.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
+  mc.on('swipeup swipedown', function(e) {
+
+    updateHelper(e);
+
+  });
+
+  // determine scroll, swipe, and arrow key direction
+  function updateHelper(param) {
+
+    var curActive = $('.side-nav').find('.is-active'),
+        curPos = $('.side-nav').children().index(curActive),
+        lastItem = $('.side-nav').children().length - 1,
+        nextPos = 0;
+
+    if (param.type === "swipeup" || param.keyCode === 40 || param > 0) {
+      if (curPos !== lastItem) {
+        nextPos = curPos + 1;
+        updateNavs(nextPos);
+        updateContent(curPos, nextPos, lastItem);
+      }
+      else {
+        updateNavs(nextPos);
+        updateContent(curPos, nextPos, lastItem);
+      }
+    }
+    else if (param.type === "swipedown" || param.keyCode === 38 || param < 0){
+      if (curPos !== 0){
+        nextPos = curPos - 1;
+        updateNavs(nextPos);
+        updateContent(curPos, nextPos, lastItem);
+      }
+      else {
+        nextPos = lastItem;
+        updateNavs(nextPos);
+        updateContent(curPos, nextPos, lastItem);
+      }
+    }
+  }
+
+  $('.intro--banner img').click(function(){
+
+    if (!($(this).hasClass('is-active'))) {
+
+      var $this = $(this),
+          curActive = $this.parent().find('.is-active'),
+          curPos = $this.parent().children().index(curActive),
+          nextPos = $this.parent().children().index($this),
+          lastItem = $(this).parent().children().length - 1;
+
+      updateNavs(nextPos);
+      updateContent(curPos, nextPos, lastItem);
+
+    }
+  });
+
+  $('.side-nav-img li').click(function(){
+
+    if (!($(this).hasClass('is-active'))) {
+
+      var $this = $(this),
+          curActive = $this.parent().find('.is-active'),
+          curPos = $this.parent().children().index(curActive),
+          nextPos = $this.parent().children().index($this),
+          lastItem = $(this).parent().children().length - 1;
+
+      updateNavs(nextPos);
+      updateContent(curPos, nextPos, lastItem);
+
+    }
+  });
+});
